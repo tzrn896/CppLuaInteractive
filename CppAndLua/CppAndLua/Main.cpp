b0d3a23c5ff0debecf91a7625847fcb3841845fd
tzrn896 <332957409@qq.com>
2020-10-11 21:35:27 +0800

    添加Lua文件

--- CppAndLua/CppAndLua/Main.cpp
+++ CppAndLua/CppAndLua/Main.cpp
@@ -1,5 +1,13 @@
 #include<iostream>
+#include<lua.hpp>
 using namespace std;
 int main() {
+	lua_State* L = luaL_newstate();
+	if (L == NULL) {
+		return 0;
+	}
+	luaL_openlibs(L);
+	luaL_dofile(L, "Lua/Supplier.lua");
+	lua_close(L);
 	return 0;
 }
